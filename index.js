3c8fa6c9fa0e5c94ed4902434a17cab1c0e8034f
Stas Vishnevetskiy <Stanislav_Vishnevetskiy@intuit.com>
2019-05-25 18:43:52 +0300

    serve frontend without be

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
 
 getData = () => {
   return {
-    api: (apikey, adultCount, maxPrice, solutions, origin, destination, date, fn) => {
-      const request = require('request');
-      const endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+apikey;
-      let data = require('data.json');
+    api: (
+      apikey,
+      adultCount,
+      maxPrice,
+      solutions,
+      origin,
+      destination,
+      date,
+      fn
+    ) => {
+      const request = require("request");
+      const endPoint =
+        "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + apikey;
+      let data = require("data.json");
 
       data.request.passengers.adultCount = adultCount;
       data.request.maxPrice = maxPrice;
@@ -15,63 +25,63 @@ getData = () => {
       data.request.slice[0].destination = destination;
       data.request.slice[0].date = date;
 
-      request({method: "post",  url: endPoint,  body: data,  json: true}, (err, resp, body) => {
+      request(
+        { method: "post", url: endPoint, body: data, json: true },
+        (err, resp, body) => {
+          let info = [];
+          let jsonObject = {};
+          const airline = "LY";
+          const price = "0";
 
-        let info = [];
-        let jsonObject = {};
-        const airline = "LY";
-        const price = "0";
-
-        if(body.error) return console.error(body.error);
-        for(i=0; i < body.trips.tripOption.length; i++){
-          airline = body.trips.tripOption[i].slice[0].segment[0].flight.carrier
-          price = body.trips.tripOption[i].saleTotal
-          jsonObject = {"airline": airline , "price": price};
-          info.push(jsonObject);
+          if (body.error) return console.error(body.error);
+          for (i = 0; i < body.trips.tripOption.length; i++) {
+            airline =
+              body.trips.tripOption[i].slice[0].segment[0].flight.carrier;
+            price = body.trips.tripOption[i].saleTotal;
+            jsonObject = { airline: airline, price: price };
+            info.push(jsonObject);
+          }
+          fn(info);
         }
-        fn(info);
-      });
+      );
     }
-  }
-}
-
+  };
+};
 
 const express = require("express");
 
 const app = express();
-app.get('/', (req,res) => {
-  res.status(200).json(data.json);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/frontend/dist/index.html");
 });
 
-app.get('/test', function (req, res) {
-  res.send('This is an test page.');
+app.get("/test", function(req, res) {
+  res.send("This is an test page.");
 });
 
-app.get('/index', function (req, res) {
- res.sendfile('./index.html');
+// app.get("/index", function(req, res) {
+//   res.sendfile("./index.html");
+// });
+
+app.get("/index-cap", function(req, res) {
+  res.sendfile("./flightCapstone/index.html");
 });
 
-app.get('/index-cap', function (req, res) {
- res.sendfile('./flightCapstone/index.html');
+app.get("/index-gal", function(req, res) {
+  res.sendfile("./public/index.html");
 });
 
-app.get('/index-gal', function (req, res) {
- res.sendfile('./public/index.html');
-});
-
-
-
-app.get('/api', (req,res) => {
-//  
-//var qpx = require("qpx")('39563a-9c3a77');
-//getData().api("39563a-9c3a77", "1", "EUR5000", "1", "DUB", "GDN", "2016-12-14", function(data){
+app.get("/api", (req, res) => {
+  //
+  //var qpx = require("qpx")('39563a-9c3a77');
+  //getData().api("39563a-9c3a77", "1", "EUR5000", "1", "DUB", "GDN", "2016-12-14", function(data){
   //console.log(data);
-  res.status(200).json({ airline: 'SK', price: 'EUR71.10' } );
+  res.status(200).json({ airline: "SK", price: "EUR71.10" });
 
   //data looks like: [ { airline: 'SK', price: 'EUR71.10' } ]   });
-//  });
+  //  });
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('app started')
+  console.log("app started at 8080");
 });
